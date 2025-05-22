12cc097056fc33b033ef91870b13ecd36af7d78e
Edrich Barnard <edrichbarnard@gmail.com>
2025-05-22 20:40:25 +0200

    feature: integrate with supabase

--- vite.config.ts
+++ vite.config.ts
@@ -8,18 +8,92 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-    Components({
-      resolvers: [PrimeVueResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({mode}) => {
+  if (mode === 'development') {
+    return {
+      plugins: [
+        vue(),
+        vueDevTools(),
+        tailwindcss(),
+        Components({
+          resolvers: [PrimeVueResolver()],
+        }),
+      ],
+      appType: 'spa',
+      build: {
+        outDir: './dist/dev',
+        sourcemap: true,
+      },
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+      },
+    }
+  } else if (mode === 'staging') {
+    return {
+      plugins: [
+        vue(),
+        vueDevTools(),
+        tailwindcss(),
+        Components({
+          resolvers: [PrimeVueResolver()],
+        }),
+      ],
+      appType: 'spa',
+      build: {
+        outDir: './dist/stage',
+        sourcemap: false,
+      },
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+      },
+    }
+  } else if (mode === 'production') {
+    return {
+      plugins: [
+        vue(),
+        vueDevTools(),
+        tailwindcss(),
+        Components({
+          resolvers: [PrimeVueResolver()],
+        }),
+      ],
+      appType: 'spa',
+      build: {
+        outDir: './dist/prod',
+        sourcemap: false,
+      },
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+      },
+    }
+  }
+
+  // default config
+  return {
+    mode: mode,
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+      Components({
+        resolvers: [PrimeVueResolver()],
+      }),
+    ],
+    appType: 'spa',
+    build: {
+      outDir: './dist/dev',
+      sourcemap: true,
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
